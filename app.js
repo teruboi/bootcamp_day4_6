487be6acf9397e39d51c6ed9dfc09fc9b0427fc7
eldrapratama <eldrapratama43@gmail.com>
2023-02-09 14:47:21 +0700

    Added contacts.json

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
-const app = express()
 const fs = require('fs')
+const morgan = require('morgan')
+
+const app = express()
 const port = 3000
 
 //Using EJS as the view engine
 app.set('view engine', 'ejs')
+
+app.use(morgan('status'))
 
 //EJS Layouts using Express-EJS-Layouts
 app.use(expressLayout)
@@ -26,19 +30,18 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res, next) => {
     // res.sendFile('./about.html', {root: __dirname})
     res.render('about', {title: 'About'})
-    next()
 })
 
 app.get('/contact', (req, res) => {
-    // res.sendFile('./contact.html', {root: __dirname})
-    const contacts = [
-        {nama: 'Reynaldi', no_telp: '081255556666'},
-        {nama: 'Tomi', no_telp: '081233334444'},
-        {nama: 'Joni', no_telp: '081277778888'},
-        {nama: 'Budi', no_telp: '081299990000'},
-    ]
+    // // res.sendFile('./contact.html', {root: __dirname})
+    // const contacts = [
+    //     {nama: 'Reynaldi', no_telp: '081255556666'},
+    //     {nama: 'Tomi', no_telp: '081233334444'},
+    //     {nama: 'Joni', no_telp: '081277778888'},
+    //     {nama: 'Budi', no_telp: '081299990000'},
+    // ]
     // const contacts = []
-    res.render('contact', {title: 'Contacts', arr: contacts})
+    res.render('contact', {title: 'Contacts', arr: (JSON.parse(fs.readFileSync('./public/contacts.json', 'utf-8')))})
 })
 
 app.get('/product/:id', (req, res) => {
