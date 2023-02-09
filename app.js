309fb06b694e10465cd4e9c4f2896c81a8641ed6
eldrapratama <eldrapratama43@gmail.com>
2023-02-09 11:13:38 +0700

    Added table in Contacts page using EJS loop function

--- app.js
+++ app.js
@@ -3,22 +3,34 @@ const app = express()
 const fs = require('fs')
 const port = 3000
 
+//Using EJS as the view engine
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.sendFile('./index.html', {root: __dirname})
+//   res.sendFile('./index.html', {root: __dirname})
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./about.html', {root: __dirname})
+    // res.sendFile('./about.html', {root: __dirname})
+    res.render('about')
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile('./contact.html', {root: __dirname})
+    // res.sendFile('./contact.html', {root: __dirname})
+    // const contacts = [
+    //     {nama: 'Reynaldi', no_telp: '081255556666'},
+    //     {nama: 'Tomi', no_telp: '081233334444'},
+    //     {nama: 'Joni', no_telp: '081277778888'},
+    //     {nama: 'Budi', no_telp: '081299990000'},
+    // ]
+    const contacts = []
+    res.render('contact', {arr: contacts})
 })
 
-app.get('/product/:prodID/category/:cat', (req, res) => {
-    const id = req.params.prodID
-    const cat = req.params.cat
-    res.send(`This ${cat} have a product id ${id}`)
+app.get('/product/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`This ${req.query.category} have a product id ${id}`)
 })
 
 app.use('/', (req, res) => {
